10599537211c194bbcae0f11560565bd6c23b846
Vrashti Goel <vrashtigoel0503@gmail.comgit config --global user.name Vrashti>
2026-09-26 21:42:39 +0530

    This is sum Code

--- practice_26sept.py
+++ practice_26sept.py
@@ -1,3 +1,11 @@
-print(1+2.5)
-name= input("Enter your name: ")
-print(name, "is learning Python everyday.")
+# print(1+2.5)
+# name= input("Enter your name: ")
+# print(name, "is learning Python everyday.")
+
+print(1+int(2.999))
+
+# SUM Program
+a= int(input("Enter a: "))
+b= int(input("Enter b: "))
+sum= a+b
+print("sum: ", sum)
